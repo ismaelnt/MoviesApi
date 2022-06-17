6ca954a4e7d25b5ef8a85b04d83e6f1c577f7c83
ismaelnt <ismaelnt97@gmail.com>
2022-06-17 08:55:31 -0300

    chore: add connection to database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
